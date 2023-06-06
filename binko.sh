3137d9a31f34dacef68b29dec781e414054543d7
pa-ljubinko <92484860+pa-ljubinko@users.noreply.github.com>
2023-06-06 15:20:52 +0200

    Update binko.sh

--- binko.sh
+++ binko.sh
@@ -38,3 +38,6 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp ~/binko-dwm/.zshrc ~/.zshrc
+cp ~/binko-dwm/.p10k.zsh ~/.p10k.zsh
